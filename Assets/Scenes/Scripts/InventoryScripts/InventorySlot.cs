1038dbf8371a16f6d465222d34f2be933523da5f
Kyo Matias <kyomatias.acad@gmail.com>
2022-12-04 11:56:56 +0800

    Updated the Inventory Slot script

--- Assets/Scenes/Scripts/InventoryScripts/InventorySlot.cs
+++ Assets/Scenes/Scripts/InventoryScripts/InventorySlot.cs
@@ -27,8 +27,35 @@ public class InventorySlot
 
     public InventorySlot()
     {
+        ClearSlot();
+    }
+
+    public void ClearSlot()
+    {
         _itemData = null;
         _stacksize = -1;
+    }
+
+    public bool RoomLeftToStack(int amountToAdd, out int amountRemaining)
+    {
+        amountRemaining = _itemData.MaxStack - _stacksize;
+        return RoomLeftInStack(amountToAdd);
+    }
+
+    public bool RoomLeftInStack(int amountToAdd)
+    {
+        if (_stacksize + amountToAdd <= _itemData.MaxStack) return true;
+        else return false;
+    }
+
+    public void AddToStack(int amount)
+    {
+        _stacksize += amount;
+    }
+
+    public void RemoveFromStack(int amount)
+    {
+        _stacksize -= amount;
     }
 }
 
